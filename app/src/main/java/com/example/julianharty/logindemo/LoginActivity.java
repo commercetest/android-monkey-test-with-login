bdb5d1698451aca9192e9fcff73de0ad22bf5cd5
Joseph Reeve <joseph.reeve@googlemail.com>
2018-05-31 15:40:26 +0100

    Fixed login page (credentials need to be correct)

--- app/src/main/java/com/example/julianharty/logindemo/LoginActivity.java
+++ app/src/main/java/com/example/julianharty/logindemo/LoginActivity.java
@@ -325,7 +325,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
 
             // TODO: register the new account here.
-            return true;
+            return false;
         }
 
         @Override
